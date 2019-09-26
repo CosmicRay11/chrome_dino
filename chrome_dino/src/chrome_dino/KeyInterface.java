07d96a949b2f96823284617324787e093eb1c6cf
CosmicRay11 <george.w11@hotmail.co.uk>
2019-09-26 16:16:54 +0100

    Initialising a robot object for the KeyInterface class

--- chrome_dino/src/chrome_dino/KeyInterface.java
+++ chrome_dino/src/chrome_dino/KeyInterface.java
@@ -1,6 +1,32 @@
 package chrome_dino;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
 public class KeyInterface {
 	
+	public static boolean main(String[] args) {
 
+		Robot r;
+		boolean works;
+		try {
+			r = new Robot();
+			//r.keyPress(KeyEvent.VK_SPACE);
+			//TimeUnit.SECONDS.sleep(2);
+			//r.keyRelease(KeyEvent.VK_SPACE);
+			works = true;
+			
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			works = false;
+		} 
+		
+		return works;
+	}
+	
+	
+	
 }
