5755da6c860ba00f43d82fa7d70a161fbf2a3dd3
CosmicRay11 <george.w11@hotmail.co.uk>
2019-09-26 15:55:11 +0100

    master class

---
+++ chrome_dino/src/chrome_dino/Play.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chrome_dino;
+
+/**
+ * @author George
+ *
+ */
+public class Play {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
